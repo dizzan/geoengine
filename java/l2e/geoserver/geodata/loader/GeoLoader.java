7bcc1cc3ee3901d33086d13361e8ff17e0006d3a
GenuinelyInsane <alphonsemarian@gmail.com>
2024-11-10 10:13:30 +0000

    Add files via upload
    DarkRise geoengine

---
+++ java/l2e/geoserver/geodata/loader/GeoLoader.java
@@ -0,0 +1,26 @@
+/*
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <www.l2darkrise.com/>.
+ */
+package l2e.geoserver.geodata.loader;
+
+import java.io.File;
+
+/**
+ * Created by GenuinelyInsane 10.09.2013 Based on L2J-DarkRise
+ */
+public interface GeoLoader {
+	public boolean isAcceptable(File file);
+
+	public GeoFileInfo readFile(File file);
+}
---
+++ java/l2e/geoserver/geodata/loader/GeoLoaderFactory.java
@@ -0,0 +1,51 @@
+/*
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <www.l2darkrise.com/>.
+ */
+package l2e.geoserver.geodata.loader;
+
+import java.io.File;
+
+/**
+ * Created by GenuinelyInsane 10.09.2013 Based on L2J-DarkRise
+ */
+public class GeoLoaderFactory {
+	private static GeoLoaderFactory instance;
+
+	private final GeoLoader[] geoLoaders;
+
+	public static GeoLoaderFactory getInstance() {
+		if (instance == null) {
+			instance = new GeoLoaderFactory();
+		}
+
+		return instance;
+	}
+
+	private GeoLoaderFactory() {
+		geoLoaders = new GeoLoader[] { new L2JGeoLoader(), new OffGeoLoader() };
+	}
+
+	public GeoLoader getGeoLoader(File file) {
+		if (file == null) {
+			return null;
+		}
+
+		for (GeoLoader geoLoader : geoLoaders) {
+			if (geoLoader.isAcceptable(file)) {
+				return geoLoader;
+			}
+		}
+		return null;
+	}
+}
